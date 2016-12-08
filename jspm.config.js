1b87b804851a8be3523464a7ffa8499a5bada81d
TxHawks <tofu.hawks@gmail.com>
2016-12-08 14:12:19 +0200

    Update htz-dispatch-event

--- jspm.config.js
+++ jspm.config.js
@@ -1,4 +1,14 @@
 SystemJS.config({
+  nodeConfig: {
+    "paths": {
+      "htz-a11y-dropdown/": "src/"
+    }
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.12"
+    }
+  },
   transpiler: "plugin-babel",
   packages: {
     "htz-a11y-dropdown": {
@@ -19,9 +29,8 @@ SystemJS.config({
     "github:*/*.json"
   ],
   map: {
-    "htz-dispatch-event": "github:haaretz/htz-dispatch-event@1.0.0",
-    "htz-get-focusables": "github:haaretz/htz-get-focusables@1.0.0",
-    "plugin-babel": "npm:systemjs-plugin-babel@0.0.12"
+    "htz-dispatch-event": "github:haaretz/htz-dispatch-event@1.0.3",
+    "htz-get-focusables": "github:haaretz/htz-get-focusables@1.0.0"
   },
   packages: {}
 });
